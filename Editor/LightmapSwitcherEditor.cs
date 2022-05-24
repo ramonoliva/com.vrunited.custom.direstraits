7463d5ede4a1d72efeba48f72ca0d9cf80f10022
Ramon <a@a.com>
2022-05-24 15:25:27 +0200

    * Added editor folder.

---
+++ Editor/LightmapSwitcherEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+
+
+[CustomEditor(typeof(LightmapSwitcher))]
+public class LightmapSwitcherEditor : Editor
+{
+
+    public ProbeData dataToFill;
+    // Start is called before the first frame update
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        dataToFill = EditorGUILayout.ObjectField("", dataToFill, typeof(ProbeData), true) as ProbeData;
+        if (GUILayout.Button("Save current Probes to asset")) 
+        { 
+            if(dataToFill == null)
+            {
+                Debug.LogWarning("Error: ProbeData asset not asigned");
+            }
+            else
+            {
+                dataToFill.SerializeLightProbes(LightmapSettings.lightProbes.bakedProbes);
+                EditorUtility.SetDirty(dataToFill);
+            }
+         }
+}
+}
